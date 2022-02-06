d9245d8ed78303538cd96b63640b501eefce71d5
Dmitry Mikushin <dmitry@kernelgen.org>
2022-02-06 15:26:03 +0000

    Bind to 0.0.0.0 by default, unless a command line argument is specified

--- config.py
+++ config.py
@@ -11,3 +11,5 @@ MAX_CONTENT_LENGTH = 128 * 1024 * 1024 # 128 MB
 BASE_UPLOAD_FOLDER = './data/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg'])
 
+SECRET_KEY = "3d2a29a5641fcfc4d5ed2be69af8391c"
+
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import sys
 
 from config import DATABASE_PATH, BASE_UPLOAD_FOLDER
 from simple_print_server import app
@@ -15,4 +16,7 @@ def create_db_if_not_created():
 
 if __name__ == "__main__":
     create_db_if_not_created()
-    app.run(host='0.0.0.0')
+    host='0.0.0.0'
+    if len(sys.argv) > 1:
+        host = sys.argv[1]
+    app.run(host=host)
